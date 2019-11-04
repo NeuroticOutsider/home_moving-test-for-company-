28089119644bad5142ac13c9c3b3315d1bb4a85d
NeuroticOutsider <klimberg-nikita@yandex.ru>
2019-11-04 17:25:17 +0300

    Main slider block done + adaptive

--- js/common.js
+++ js/common.js
@@ -3,5 +3,16 @@ $(function() {
 		$(this).toggleClass('is-active');
 		$('.navbar').toggleClass('navbar--show');
 	});
+
+	$('.main-carousel').owlCarousel({
+		loop: true,
+		items: 1,
+		nav: false,
+		smartSpeed: 700,
+		responsiveClass: true,
+		dots: false, 
+		autoplay: true,
+    autoplayTimeout: 3000
+	});
 });
 
